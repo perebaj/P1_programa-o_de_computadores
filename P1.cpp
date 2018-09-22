7912b4e66417189ae0d08b8ae1a178095451592a
Jonathan <perebaj@gmail.com>
2018-09-22 14:02:17 -0300

    funfando

--- P1.cpp
+++ P1.cpp
@@ -2,46 +2,6 @@
 #include <string>
 using namespace std;
 
-class Fila
-{
-  private:
-    static int total_filas;
-    static int total_elementos;
-    int tamanho;
-    int elemento = 0;
-    string *nm;
-    string nome; //  nome da pessoa que será adicionada na fila;
-
-  public:
-    Fila(int _tam = 0, string a = "Anônimo");
-    ~Fila();
-    void set_tamanho(int);
-    void set_retira_elemento();
-    void set_inserir_elemento(string a);
-    static int get_total_filas();
-    static int get_total_elementos();
-    void name(string);
-    void print() const;
-    int get_elemento() const
-    {
-        return elemento;
-    }
-    int get_tamanho() const
-    {
-        return tamanho;
-    }
-    void imprime_vetor() const{
-        for(int i = 0; i < get_tamanho(); i++){
-            cout << nm[i] << endl;
-        }
-    }
-};
-
-/*
-Obj: valida o tamanho do vetor de string e se for válido aloca-o dinamicamente
-@param  _tam    tamanho do vetor
-*/
-
 void Fila::set_tamanho(int _tamanho)
 {
     if (_tamanho > 0)
@@ -141,41 +101,4 @@ void Fila::print() const
     cout << "Elementos da Fila " << get_elemento() << endl;
 }
 
-int main()
-{
-    Fila f1(10, "Jose");
-
-    int aux = 5;
-    cout << "1) Inserir alguem na fila" << endl;
-    cout << "2) Retirar alguem da fila" << endl;
-    cout << "0) Sair" << endl;
-    while (aux != 0)
-    {
-        cin >> aux;
-        switch (aux)
-        {
-            case 1:
-            {
-                cout << "Digite o nome da pessoa que quer inserir:" << endl;
-                string a;
-                cin >> a;
-                f1.set_inserir_elemento(a);
-                f1.print();
-                break;
-            }
-            case 2:
-            {
-                f1.set_retira_elemento();
-                f1.print();
-
-                break;
-            }
-        }
-    }
-    f1.imprime_vetor();
-
-
-    return 0;
-   
-}
 
