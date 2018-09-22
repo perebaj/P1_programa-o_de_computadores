50256e5cffae025bbc726700f262ca13fa43e380
Jonathan <perebaj@gmail.com>
2018-09-22 13:40:23 -0300

    !!

--- P1.cpp
+++ P1.cpp
@@ -108,39 +108,4 @@ void Fila::print() const
     cout << "Numero Total de Elementos: " << Fila::get_total_elementos() << endl;
     cout << "Elementos da Fila " << get_elemento() << endl;
 }
-int main()
-{
-    Fila f1(10, "Jose");
 
-    int aux = 5;
-    cout << "1) Inserir alguem na fila" << endl;
-    cout << "2) Retirar alguem da fila" << endl;
-    cout << "0) Sair" << endl;
-    while (aux != 0)
-    {
-        cin >> aux;
-        switch (aux)
-        {
-            case 1:
-            {
-                cout << "Digite o nome da pessoa que quer inserir:" << endl;
-                string a;
-                cin >> a;
-                f1.set_inserir_elemento(a);
-                f1.print();
-                break;
-            }
-            case 2:
-            {
-                f1.set_retira_elemento();
-                f1.print();
-
-                break;
-            }
-        }
-    }
-    f1.imprime_vetor();
-
-
-    return 0;
-}
